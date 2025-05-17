76a5ecf6a3bfbee14d7e2aeb044ded74e4362205
Rubens André Echeverria <rapresa@uninorte.edu.co>
2025-05-17 09:20:11 -0500

    ?

--- src/screens/categoryForm.js
+++ src/screens/categoryForm.js
@@ -14,7 +14,7 @@ export default function CategoryForm() {
     iconDropdownProps,
     handleInputChange,
     handleSubmit
-  } = useCategoryForm() // TODO: this doesnt exist yet estoy en la rama correcta ola?
+  } = useCategoryForm() // TODO: this doesnt exist yet 
   
   return (
     <ScrollView
