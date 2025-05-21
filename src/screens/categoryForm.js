a35cf9b82c158b02977b367b3403f67f5b1f103d
Lena Castillo <lenac@uninorte.edu.co>
2025-05-21 15:46:14 -0500

    Enhance LoadingScreen with improved layout and activity indicator

--- src/screens/categoryForm.js
+++ src/screens/categoryForm.js
@@ -85,10 +85,7 @@ export default function CategoryForm() {
     >
       <View className="flex-1 pb-36">
         <View className="bg-pink-800 h-60 rounded-br-[65px]">
-          <View className="bg-pink-100 h-60 w-60 rounded-full absolute top-[-90px] right-[-79px]" />
-          <View className="bg-pink-600 h-36 w-36 rounded-full absolute top-[-42px] right-[-34px]" />
-          <View className="bg-pink-500 h-10 w-10 rounded-full absolute top-28 right-3" />
-          <View className="bg-pink-300 h-20 w-20 rounded-full absolute top-3 right-32" />
+       
           <View className="w-10/12 flex-1 absolute bottom-7 left-5">
             <InputComponent
               name="label"
